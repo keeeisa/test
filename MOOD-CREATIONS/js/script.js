d862e0e352c298be8c4e9db3b2acc5942a4f4c57
KONSTANTINS-PC\user <konstantins@digbrand.lv>
2017-06-13 17:28:11 +0300

    -updates

--- MOOD-CREATIONS/js/script.js
+++ MOOD-CREATIONS/js/script.js
@@ -12,30 +12,43 @@
 //     }
 //   });
 // });
-var form = document.querySelector("#contact-form");
+function $(selector){
+  return document.querySelector(selector);
+}
+function $$(selector){
+  return document.querySelectorAll(selector);
+}
+var form = $("#contact-form");
 console.log(form);
   form.onsubmit = function(e){
     e.preventDefault()
     alert("Its Alive!")
 
-    var input_1 = document.querySelector("#js-input-1");
-    console.log(input_1.value);
+    var inputs = $$(".input");
+    for (var i = 0; i < inputs.length; i++){
+      console.log(inputs[i].value);
 
-    var input_2 = document.querySelector("#js-input-2");
-    console.log(input_2.value);
+      if(inputs[i].value.trim() === ""){
+      console.error("Kļūda");
+      }
+    };
 
-    var textarea = document.querySelector("#js-textarea");
-    console.log(textarea.value);
-
-    if(input_1.value === ""){
-      console.error("Kļūda!");
-    };
-    if(input_2.value === "" || input_2.value.indexOf("@") === -1){
-      console.error("Kļūda!");
-    };
-    if(textarea.value === ""){
-      console.error("Kļūda!");
-    };
-    // var str = input_2.value;
-    // var n = str.includes("@");
-};
+    // var input_1 = $("#js-input-1");
+    // console.log(input_1.value);
+    //
+    // var input_2 = $("#js-input-2");
+    // console.log(input_2.value);
+    //
+    // var textarea = $("#js-textarea");
+    // console.log(textarea.value);
+    //
+    // if(input_1.value.trim() === ""){
+    //   console.error("Kļūda!");
+    // };
+    // if(input_2.value === "" || input_2.value.indexOf("@") === -1){
+    //   console.error("Kļūda!");
+    // };
+    // if(textarea.value === ""){
+    //   console.error("Kļūda!");
+    // };
+  };
